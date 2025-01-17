ed0a4664bfff727a68ace7802e701374ead385db
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-01-17 14:43:42 +0100

    fevrr

---
+++ script.js
@@ -0,0 +1,8 @@
+let ett = document.querySelector("#ett");
+let två = document.querySelector("#två");
+
+ett = parseInt("#ett");
+två = parseInt("#två");
+
+let svar = document.querySelector(".svar");
+svar = ett + två;
