18259f02f05a2fb66afb3fbc1fca3c58f8bb597b
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-01-20 14:32:40 +0100

    klar

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-let ett = document.querySelector("#ett");
-let två = document.querySelector("#två");
+function add() {
+  let ett = parseInt(document.querySelector("#ett").value);
+  let två = parseInt(document.querySelector("#två").value);
+  let output_input = document.querySelector("#output");
 
-ett = parseInt("#ett");
-två = parseInt("#två");
+  let output = parseInt(output_input.value);
+  output_input.value = ett + två;
+}
+function sub() {
+  let ett = parseInt(document.querySelector("#ett").value);
+  let två = parseInt(document.querySelector("#två").value);
+  let output_input = document.querySelector("#output");
 
-let svar = document.querySelector(".svar");
-svar = ett + två;
+  let output = parseInt(output_input.value);
+  output_input.value = ett - två;
+}
+function mul() {
+  let ett = parseInt(document.querySelector("#ett").value);
+  let två = parseInt(document.querySelector("#två").value);
+  let output_input = document.querySelector("#output");
+
+  let output = parseInt(output_input.value);
+  output_input.value = ett * två;
+}
+function div() {
+  let ett = parseInt(document.querySelector("#ett").value);
+  let två = parseInt(document.querySelector("#två").value);
+  let output_input = document.querySelector("#output");
+
+  let output = parseInt(output_input.value);
+  output_input.value = ett / två;
+}
+
+let städer = ["tirana", "bucarest"];
+let länder = ["albanien", "rumänien"];
+let slump = parseInt(Math.random() * 7);
